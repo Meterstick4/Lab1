0d8c9488ad3b827f2f77a9b24bd00fa5fa19439b
TtMoS <38534822+TtMoS@users.noreply.github.com>
2018-04-26 09:09:21 -0500

    scipts
    s

---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class WorldController : MonoBehaviour 
+{
+	public Object[] objects;
+	void Start () 
+	{
+		DontDestroyOnLoad (GameObject.Find ("GameObject"));
+
+		objects = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(SceneManager.GetActiveScene().buildIndex == 0)
+		{
+			objects = GameObject.FindGameObjectsWithTag("Player");
+			if(objects.Length > 1)
+			{
+				Destroy (objects [1]);
+			}
+
+		}
+		else if(SceneManager.GetActiveScene().buildIndex == 1)
+		{
+			objects = GameObject.FindGameObjectsWithTag("Player");
+			if(objects.Length > 1)
+			{
+				Destroy (objects [1]);
+			}
+		}
+
+
+
+	}
+}
